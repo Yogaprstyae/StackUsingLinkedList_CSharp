a963c6fb84e70a22b3f76ebbe0bd795a69786541
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-11-30 18:41:43 +0700

    Add method to run the program and change method name

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -21,7 +21,7 @@ namespace StackUsingLinkedList_CSharp
     {
         Node top;
 
-        public Stack()
+        public Stacks()
         {
             top = null;
         }
@@ -64,6 +64,47 @@ namespace StackUsingLinkedList_CSharp
                     Console.WriteLine(tmp.info);
                 }
                 Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop.");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choise: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        Console.WriteLine("------- Display -------");
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
             }
         }
     }
