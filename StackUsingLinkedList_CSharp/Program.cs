8d84a8ddf10a6a7315e66d49ab7426aabff64909
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-11-30 16:45:25 +0700

    Add method display

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -49,5 +49,22 @@ namespace StackUsingLinkedList_CSharp
             Console.WriteLine("\nThe popped element is: " + top.info);
             top = top.next; //Make top point to the next node is sequence
         }
+
+        public void display()
+        {
+            Node tmp;
+
+            if (empty()) //If stack is empty
+                Console.WriteLine("\nStack is empty");
+            else
+            {
+                //Traverse the list form beggining till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
